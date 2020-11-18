e0210e96a6b02460e739b7f169fd8b1cbd1ecf92
WeeMick <michaelblr5@gmail.com>
2020-11-18 23:51:45 +0000

    additional movie DB fields added

--- src/Movie/MovieBundle/Entity/Movie.php
+++ src/Movie/MovieBundle/Entity/Movie.php
@@ -30,7 +30,65 @@ class Movie
     }
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", length=100)
      */
     protected $title;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    protected $description;
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * @param mixed $rating
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+    }
+
+    /**
+     * @ORM\Column(type="decimal", scale=1, precision=2)
+     */
+    protected $rating;
 }
