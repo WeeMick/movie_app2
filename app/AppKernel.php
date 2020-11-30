1d62c6e3d69582d48ecdb23c787c348f435389f0
WeeMick <michaelblr5@gmail.com>
2020-11-30 19:17:14 +0000

    added fos user functionality, login route working

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Movie\MovieBundle\MovieMovieBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle()
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
