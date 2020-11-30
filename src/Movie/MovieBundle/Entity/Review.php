e5b4ab2c61cc94f7ad4d21fc82a66b36fdb22b3a
WeeMick <michaelblr5@gmail.com>
2020-11-30 19:00:10 +0000

    review entity added

---
+++ src/Movie/MovieBundle/Entity/Review.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Movie\MovieBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Movie
+ * @package Movie\MovieBundle\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="review")
+ */
+
+class Review
+{
+    /**
+     * @return mixed
+     */
+    public function getReviewId()
+    {
+        return $this->review_id;
+    }
+
+    /**
+     * @param mixed $review_id
+     */
+    public function setReviewId($review_id)
+    {
+        $this->review_id = $review_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMovieId()
+    {
+        return $this->movie_id;
+    }
+
+    /**
+     * @param mixed $movie_id
+     */
+    public function setMovieId($movie_id)
+    {
+        $this->movie_id = $movie_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReviewerId()
+    {
+        return $this->reviewer_id;
+    }
+
+    /**
+     * @param mixed $reviewer_id
+     */
+    public function setReviewerId($reviewer_id)
+    {
+        $this->reviewer_id = $reviewer_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReview()
+    {
+        return $this->review;
+    }
+
+    /**
+     * @param mixed $review
+     */
+    public function setReview($review)
+    {
+        $this->review = $review;
+    }
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $review_id;
+
+    /**
+     * @ORM\ManyToOne( targetEntity="Movie\MovieBundle\Entity\Movie")
+     * @ORM\JoinColumn (referencedColumnName="id")
+     */
+    protected $movie_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $reviewer_id;
+
+    /**
+     * @ORM\Column(type="string", length=700)
+     */
+    protected $review;
+}
