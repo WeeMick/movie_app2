8d9aa3e48cd0975390764fbf533f57f4637a3770
WeeMick <michaelblr5@gmail.com>
2021-05-06 12:44:10 +0100

    consumed API tidied and outputting details I want

--- src/Movie/MovieBundle/Controller/OMDBController.php
+++ src/Movie/MovieBundle/Controller/OMDBController.php
@@ -11,6 +11,18 @@ use GuzzleHttp\Exception\GuzzleException;
 
 class OMDBController extends AbstractFOSRestController
 {
+
+    private $api_key;
+    private $movie_title;
+    private $movie_id;
+
+    public function __construct()
+    {
+        $this->api_key = "&apikey=61444435";
+        $this->movie_title = "Aladdin";
+        $this->movie_id = "tt1285016"; // This id relates to "The Social Network
+    }
+
     /**
      * @Route("/film")
      * @return mixed
@@ -20,32 +32,56 @@ class OMDBController extends AbstractFOSRestController
     public function getFilmByIdAction()
     {
         $client = new Client(['base_uri' => 'http://www.omdbapi.com/']);
-        $api_key = '&apikey=61444435';
-        //example id = tt1285016
-        $id = 'tt1285016';
-        $request = $client->request('GET', '?i=' . $id . $api_key);
+        $request = $client->request('GET', '?i=' . $this->movie_id . $this->api_key);
         $movie = json_decode($request->getBody(), true);
 
         return $this->handleView($this->view($movie));
+
+        // try to add this data to post action to create a new movie for my
     }
+
+    /**
+     * @Route("/filmyear")
+     * @return mixed
+     * @throws GuzzleException
+     *
+     */
+    public function getFimYearAction()
+    {
+        $client = new Client(['base_uri' => 'http://www.omdbapi.com/']);
+        $request = $client->request('GET', '?i=' . $this->movie_id . $this->api_key);
+        $movie_array = json_decode($request->getBody(), true);
+
+        foreach ($movie_array as $key => $value) {
+            if($key === "Year")
+            {
+                echo $value;
+            }
+        }
+
+    die();
+//        return $this->handleView($this->view($movie_array));
+
+        // try to add this data to post action to create a new movie for my
+    }
+
 
     /**
      * @Route("/year")
      * @return mixed
      * @throws GuzzleException
-     *
      */
-    public function getYearOfMovieAction()
+    public function getYearAction()
     {
         $client = new Client(['base_uri' => 'http://www.omdbapi.com/']);
         $api_key = '&apikey=61444435';
+//        $movieTitle = "Aladdin";
+        $yearOfRelease = 1996;
+        $request = $client->request('GET', '?y=' . $yearOfRelease . $api_key);
+        $movies = json_decode($request->getBody(), true);
 
-        $title = "Aladdin";
-        $request = $client->request('GET', '?s=' . $title . $api_key);
-        $year = json_decode($request->getBody(), true);
 
-
-        return $this->handleView($this->view($year));
+        return $this->handleView($this->view($movies));
     }
 
     /**
@@ -57,12 +93,17 @@ class OMDBController extends AbstractFOSRestController
     public function getActorsAction()
     {
         $client = new Client(['base_uri' => 'http://www.omdbapi.com/']);
-        $api_key = '&apikey=61444435';
+        $request = $client->request('GET', '?i=' . $this->movie_id . $this->api_key);
+        $movie_array = json_decode($request->getBody(), true);
 
-        $title = "Pirates of the Caribbean";
-        $request = $client->request('GET', '?s=' . $title . $api_key);
-        $movie = json_decode($request->getBody(), true);
-
-        return $this->handleView($this->view($movie));
+        foreach ($movie_array as $key => $value) {
+            if($key === "Actors")
+            {
+                $output = $value;
+                echo $value;
+            }
+        }
+//        die();
+        return $this->handleView($this->view($output));
     }
 }
